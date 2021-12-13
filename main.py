d483ca9fc97c1ad65391b7d4536004dfdd2a4ac9
khadada <lkhadada@gmail.com>
2021-12-13 07:51:39 +0100

    I add fuctionality to clear button

--- main.py
+++ main.py
@@ -10,8 +10,14 @@ WORK_MIN = 1
 SHORT_BREAK_MIN = 1
 LONG_BREAK_MIN = 2
 reps = 0
+time = None
 # ---------------------------- TIMER RESET ------------------------------- # 
-
+def clear_all():
+    global reps
+    root.after_cancel(time)
+    reps = 0
+    timer_label.config(text="Timer")
+    canvas.itemconfig(text_canvas,text="00:00")
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_count_down():
     global reps
@@ -27,8 +33,9 @@ def start_count_down():
     else:
         count_down(WORK_MIN * 60)
         timer_label.config(text="Work session")
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+# ---------------------------- COUNTD0OWN MECHANISM ------------------------------- # 
 def count_down(count):
+    global time
     minuts = math.floor(count/60)
     seconds = count % 60
     if minuts < 10:
@@ -37,12 +44,12 @@ def count_down(count):
         seconds = f'0{seconds}'
     canvas.itemconfig(text_canvas, text=f'{minuts}:{seconds}')
     if count > 0:
-        root.after(1000,count_down, count-1)
-        print(count)
+        time = root.after(1000,count_down, count-1)
     else:
         start_count_down()
         marks = ""
         mark_session = math.floor(reps/2)
+        print(range(mark_session))
         for _ in range(mark_session):
             marks+="✓"
         check_marks.config(text=marks)
@@ -70,7 +77,7 @@ check_marks.grid(row=3,column=1)
 
 start_btn = Button(text='Start',pady=5,padx=9,command=start_count_down)
 start_btn.grid(row=2,column=0)
-reset_btn = Button(text='Rest',pady=5,padx=9)
+reset_btn = Button(text='Rest',pady=5,padx=9,command=clear_all)
 reset_btn.grid(row=2,column=2)
 
 
